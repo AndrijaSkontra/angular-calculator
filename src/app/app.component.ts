0c83d795952d38c4808da0667759d07b2e93b612
AndrijaSkontra <askontra22@student.unizd.hr>
2024-04-24 11:25:24 +0200

    calculator body genarated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CalculatorBodyComponent } from './calculator-body/calculator-body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, CalculatorBodyComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/calculator-body/calculator-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator-body.component.html',
+  styleUrl: './calculator-body.component.scss'
+})
+export class CalculatorBodyComponent {
+
+}
